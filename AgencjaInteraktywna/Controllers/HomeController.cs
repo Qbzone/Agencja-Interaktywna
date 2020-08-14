5f6884ca78a0b03eeb61f4d60fd51b88530acca2
Qbzone <58001410+Qbzone@users.noreply.github.com>
2020-08-14 14:28:36 +0200

    Controller 1.0

--- AgencjaInteraktywna/Controllers/HomeController.cs
+++ AgencjaInteraktywna/Controllers/HomeController.cs
@@ -28,6 +28,12 @@ namespace AgencjaInteraktywna.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
