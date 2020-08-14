d68bc45674c88d281546f135133a07712e056cdf
Qbzone <58001410+Qbzone@users.noreply.github.com>
2020-08-14 13:53:53 +0200

    Commit

--- AgencjaInteraktywna/Models/Pakietusluga.cs
+++ AgencjaInteraktywna/Models/Pakietusluga.cs
@@ -1,25 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AgencjaInteraktywna.Models
 {
-    [Table("pakietusluga")]
     public partial class Pakietusluga
     {
-        [Key]
-        [Column("idpakiet")]
         public int Idpakiet { get; set; }
-        [Key]
-        [Column("idusluga")]
         public int Idusluga { get; set; }
 
-        [ForeignKey(nameof(Idpakiet))]
-        [InverseProperty(nameof(Pakiet.Pakietusluga))]
         public virtual Pakiet IdpakietNavigation { get; set; }
-        [ForeignKey(nameof(Idusluga))]
-        [InverseProperty(nameof(Usluga.Pakietusluga))]
         public virtual Usluga IduslugaNavigation { get; set; }
     }
 }
