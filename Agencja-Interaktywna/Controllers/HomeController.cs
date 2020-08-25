49bcde00621613cc55507cd3a4236a12e0972fdb
Qbzone <58001410+Qbzone@users.noreply.github.com>
2020-08-25 16:08:13 +0200

    Forms Update

--- Agencja-Interaktywna/Controllers/HomeController.cs
+++ Agencja-Interaktywna/Controllers/HomeController.cs
@@ -85,6 +85,11 @@ namespace Agencja_Interaktywna.Controllers
             return View();
         }
 
+        public IActionResult ForgottenPassword()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
