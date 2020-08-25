f1f0838aa41aa4cf4c04c03bb7626c40b8534ff0
Qbzone <58001410+Qbzone@users.noreply.github.com>
2020-08-25 14:48:47 +0200

    Password Validation

--- Agencja-Interaktywna/Models/Osoba.cs
+++ Agencja-Interaktywna/Models/Osoba.cs
@@ -25,11 +25,11 @@ namespace Agencja_Interaktywna.Models
         [MaxLength(50)]
         public string AdresEmail { get; set; }
         [Required(ErrorMessage = "Proszę wprowadzić hasło")]
-        [MinLength(6, ErrorMessage = "Hasło musi posiadać co najmniej 6 znaków")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W]).{6,}$", ErrorMessage = "Hasło musi posiadać conajmniej jedną małą literę, jedną dużą literę, jeden znak liczbowy, jeden znak specjalny oraz składać się z co najmniej 6 znaków.")]
         public string Haslo { get; set; }
         [NotMapped]
         [Required(ErrorMessage = "Proszę potwierdzić hasło")]
-        [MinLength(6, ErrorMessage = "Hasło musi posiadać co najmniej 6 znaków")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W]).{6,}$", ErrorMessage = "Hasło musi posiadać conajmniej jedną małą literę, jedną dużą literę, jeden znak liczbowy, jeden znak specjalny oraz składać się z co najmniej 6 znaków.")]
         [Compare("Haslo", ErrorMessage = "Hasła muszą być takie same")]
         public string PotwierdzHaslo { get; set; }
         public bool CzyEmailZweryfikowane { get; set; }
