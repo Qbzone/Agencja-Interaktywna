d5135743fc3cad39cdde36d9b3c3728d94fbb360
Qbzone <borowskij97@gmail.com>
2021-03-19 16:04:26 +0100

    Dodano widoki dla Szefa i metody w SzefController
    -Dodano pełen zakres widoków dla szefa
    -Część z nich została wypełniona wymaganą treścią
    -Dla w pełni wykonanych widoków dodano obsługujące je metody
    -Utworzono layout dla Szefa
    -Rozpoczęto prace nad funkcjonalnością tworzenia projektów.

--- Agencja-Interaktywna/Controllers/SzefController.cs
+++ Agencja-Interaktywna/Controllers/SzefController.cs
@@ -1,17 +1,176 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Agencja_Interaktywna.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Agencja_Interaktywna.Controllers
 {
     [Authorize(Roles = "Szef")]
     public class SzefController : Controller
     {
+        private readonly s16693Context _s16693context = new s16693Context();
         public IActionResult Index()
+        {
+            ViewBag.userEmail = HttpContext.User.Identity.Name;
+
+            var pr = _s16693context.Projekt.ToList();
+
+            return View(pr);
+        }
+
+        public IActionResult ProjectDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = _s16693context.Projekt
+            .FirstOrDefault(e => e.IdProjekt == id);
+
+            var tasks = _s16693context.ZadanieProjekt
+                .Include(p => p.IdProjektNavigation)
+                .Include(z => z.IdZadanieNavigation)
+                .Where(e => e.IdProjekt == id)
+                .ToList();
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(new Tuple<Projekt, List<ZadanieProjekt>>(project, tasks));
+            }
+        }
+
+        public IActionResult ProjectCreate()
+        {
+            return View();
+        }
+
+        public IActionResult ProjectEdit()
+        {
+            return View();
+        }
+
+        public IActionResult ProjectDelete()
+        {
+            return View();
+        }
+
+        public IActionResult Contract(int? id)
+        {
+            var contract = _s16693context.ProjektPakiet
+                .Include(pr => pr.IdProjektNavigation)
+                .Include(pa => pa.IdPakietNavigation)
+                .Where(x => x.IdProjekt == id)
+                .OrderByDescending(e => e.IdPakiet)
+                .ToList();
+
+            return View(contract);
+        }
+
+        public IActionResult Meetings()
+        {
+            return View();
+        }
+
+        public IActionResult MeetingsCreate()
+        {
+            return View();
+        }
+
+        public IActionResult MeetingsEdit()
+        {
+            return View();
+        }
+
+        public IActionResult MeetingsDelete()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            var sz = _s16693context.Pracownik
+                .Include(o => o.IdPracownikNavigation)
+                .FirstOrDefault(i => i.IdPracownikNavigation.AdresEmail == HttpContext.User.FindFirst(ClaimTypes.Name).Value);
+
+            if (sz == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(sz);
+            }
+        }
+
+        public IActionResult Teams()
+        {
+            return View();
+        }
+
+        public IActionResult Team(int? id)
+        {
+            var team = _s16693context.ZespolProjekt
+                .Include(p => p.IdProjektNavigation)
+                .Include(z => z.IdZespolNavigation)
+                .FirstOrDefault(x => x.IdProjekt == id);
+
+            var members = _s16693context.PracownikZespol
+                .Include(z => z.IdZespolNavigation)
+                .Include(p => p.IdPracownikNavigation)
+                    .ThenInclude(o => o.IdPracownikNavigation)
+                    .Where(x => x.IdZespol == team.IdZespol)
+                    .ToList();
+
+            return View(members);
+        }
+
+        public IActionResult TeamCreate()
+        {
+            return View();
+        }
+
+        public IActionResult TeamEdit()
+        {
+            return View();
+        }
+
+        public IActionResult TeamDelete()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult TaskDetails()
+        {
+            return View();
+        }
+
+        public IActionResult TaskCreate()
+        {
+            return View();
+        }
+
+        public IActionResult TaskEdit()
+        {
+            return View();
+        }
+
+        public IActionResult TaskDelete()
         {
             return View();
         }
--- Agencja-Interaktywna/SQL/Insert.sql
+++ Agencja-Interaktywna/SQL/Insert.sql
@@ -1,12 +1,13 @@
 ﻿INSERT INTO Osoba
-VALUES ('Jan','Kuna','595958958','123456789','jankuna@wp.pl','QWER123!@','true','62ebf753-dfd9-4e45-ad50-ae9174fece3e','Programista'); 
+VALUES ('Jan','Kuna','595958958','123456789','jankuna@wp.pl','VutSf+LocAw3LnENngk6ETkPx88C2wUqcjP+DcptQbM=','true','62ebf753-dfd9-4e45-ad50-ae9174fece3e','Programista'); 
 INSERT INTO Osoba
-VALUES ('Maciej','Lubicz','595958978','123451789','lubiczmaciej@wp.pl','QWER128!@','true','62ebf753-dfd9-4e45-ad50-ae9174fece3a','Tester'); 
+VALUES ('Maciej','Lubicz','595958978','123451789','lubiczmaciej@wp.pl','VutSf+LocAw3LnENngk6ETkPx88C2wUqcjP+DcptQbM=','true','62ebf753-dfd9-4e45-ad50-ae9174fece3a','Tester'); 
 INSERT INTO Osoba
-VALUES ('Bartosz','Bartomski','115958958','123956789','bartoszbart@wp.pl','QWER188!@','true','62ebf753-dfd9-4e45-ad50-ae9174fece3c','Grafik'); 
+VALUES ('Bartosz','Bartomski','115958958','123956789','bartoszbart@wp.pl','VutSf+LocAw3LnENngk6ETkPx88C2wUqcjP+DcptQbM=','true','62ebf753-dfd9-4e45-ad50-ae9174fece3c','Grafik'); 
 INSERT INTO Osoba
-VALUES ('Jerzy','Kozak','595951958','923456789','jerzykozak@wp.pl','QWER773!@','true','62ebf753-dfd9-4e45-ad50-ae9174fece3f','Pozycjoner'); 
-
+VALUES ('Jerzy','Kozak','595951958','923456789','jerzykozak@wp.pl','VutSf+LocAw3LnENngk6ETkPx88C2wUqcjP+DcptQbM=','true','62ebf753-dfd9-4e45-ad50-ae9174fece3f','Pozycjoner'); 
+INSERT INTO Osoba
+VALUES ('Andrzej','Tama','595911958','923466789','AndziejTamownik@wp.pl','VutSf+LocAw3LnENngk6ETkPx88C2wUqcjP+DcptQbM=','true','62ebf753-dfd9-4e45-ad50-ae9174fece4f','Szef'); 
 
 INSERT INTO Pracownik
 VALUES (2,'Warszawa ul.Wschodnia 10',8000,300,'12345678910',3);
@@ -16,6 +17,8 @@ INSERT INTO Pracownik
 VALUES (4,'Warszawa ul.Zachodnia 15',8200,200,'12345678911',5);
 INSERT INTO Pracownik
 VALUES (5,'Warszawa ul.Poludniowa 20',2000,500,'12345678913',1);
+INSERT INTO Pracownik
+VALUES (6,'Warszawa ul.Poludniowa 20',2000,500,'12345678913',6);
 
 INSERT INTO Programista
 VALUES (2,'senior');
@@ -25,6 +28,8 @@ INSERT INTO Tester
 VALUES (3,2);
 INSERT INTO Pozycjoner
 VALUES (5,'marketing szeptany');
+INSERT INTO Szef
+VALUES (6);
 
 INSERT INTO Zespol
 VALUES ('Zespol ogorkow');
