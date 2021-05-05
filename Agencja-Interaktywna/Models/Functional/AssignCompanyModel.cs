241308a0c67f9ed0908a4f920280ec581e0380d6
Kewinejro <58001489+Kewinejro@users.noreply.github.com>
2021-05-05 13:43:46 +0200

    Uzupełnienie walidacji dla wszystkich widoków + poprawki graficzne
    -Została dodana walidacja widoków dla pracowników i klienta
    -Zostały wprowadzone poprawki dla przycisków "wstecz" pod kątem programistycznym oraz wizualnym.

--- Agencja-Interaktywna/Models/Functional/AssignCompanyModel.cs
+++ Agencja-Interaktywna/Models/Functional/AssignCompanyModel.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Agencja_Interaktywna.Models.Functional
 {
-    public class AssignCompanyModel
+    public class AssignCompanyModel : IValidatableObject
     {
         public Klient klient { get; set; }
 
         public Firma firma { get; set; }
+
+        [NotMapped]
+        public string ErrorHandler1 { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            if (firma.Nazwa == null)
+            {
+                errors.Add(new ValidationResult($"Proszę wprowadzić nazwę firmy.", new List<string> { nameof(ErrorHandler1) }));
+            }
+            return errors;
+        }
     }
 }
